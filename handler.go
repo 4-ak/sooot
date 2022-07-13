66dfb0a0644adf2c301655d89d1e110ec11bb265
green-21 <133c96d@naver.com>
2022-07-13 13:31:42 +0900

    회원가입 최소기능(db 반영) 완성

--- handler.go
+++ handler.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/4-ak/sooot/db"
 	"github.com/4-ak/sooot/domain"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
@@ -22,6 +25,8 @@ func NewServer(courses *domain.CourseList) *Server {
 
 	server.App.Get("/", server.ViewCourses())
 	server.App.Get("/login", server.LoginPage())
+	server.App.Get("/registration", server.RegistrationPage())
+	server.App.Post("/registration", server.Registration())
 	return &server
 }
 
@@ -37,5 +42,38 @@ func (s *Server) ViewCourses() fiber.Handler {
 func (s *Server) LoginPage() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		return c.Render("login", nil)
+	}
+}
+
+func (s *Server) Login() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		return c.SendStatus(200)
+	}
+}
+
+func (s *Server) RegistrationPage() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		return c.Render("registration", nil)
+	}
+}
+
+func (s *Server) Registration() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var user struct {
+			EmailID string `form:"email_id"`
+			Pass    string `form:"pass"`
+		}
+		if err := c.BodyParser(&user); err != nil {
+			fmt.Println(err)
+			return nil
+		}
+
+		if _, err := db.DB.Exec("INSERT INTO user(id, pass, is_cert) VALUES(?,?,1)", user.EmailID, user.Pass); err != nil {
+			fmt.Println("회원가입 실패")
+			fmt.Println(err)
+			// 실패를 알리는 메시지
+		}
+		c.Location("/login")
+		return c.SendStatus(201)
 	}
 }
