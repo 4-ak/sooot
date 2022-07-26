f6354d7b1d13b14b86351e9609f2e5522ca8446b
JoonHyug <hyug0819@gmail.com>
2022-07-26 20:56:10 +0900

    dbhelper에 user_id 추가

--- db/dbhelper.go
+++ db/dbhelper.go
@@ -25,35 +25,37 @@ func NewDB() {
 const (
 	createLectureTable = `
 	CREATE TABLE IF NOT EXISTS "lecture" (
-		"uid"	INTEGER NOT NULL UNIQUE,
-		"name"	TEXT NOT NULL,
+		"uid"				INTEGER NOT NULL UNIQUE,
+		"name"				TEXT NOT NULL,
 		"professor_name"	TEXT NOT NULL,
-		"season"	INTEGER NOT NULL,
-		"grade"	INTEGER NOT NULL,
-		"credit"	INTEGER NOT NULL,
-		"category"	NUMERIC NOT NULL,
+		"season"			INTEGER NOT NULL,
+		"grade"				INTEGER NOT NULL,
+		"credit"			INTEGER NOT NULL,
+		"category"			NUMERIC NOT NULL,
 		PRIMARY KEY("uid" AUTOINCREMENT)
 	);
 	`
 	createReviewTable = `
 	CREATE TABLE IF NOT EXISTS "review" (
-		"uid"	INTEGER NOT NULL UNIQUE,
-		"lecture_id"	INTEGER NOT NULL,
+		"uid"				INTEGER NOT NULL UNIQUE,
+		"lecture_id"		INTEGER NOT NULL,
 		"beneficial_point"	INTEGER NOT NULL CHECK("beneficial_point" BETWEEN 1 AND 5),
-		"honey_point"	INTEGER NOT NULL CHECK("honey_point" BETWEEN 1 AND 5),
+		"honey_point"		INTEGER NOT NULL CHECK("honey_point" BETWEEN 1 AND 5),
 		"professor_point"	INTEGER NOT NULL CHECK("professor_point" BETWEEN 1 AND 5),
-		"is_team"	INTEGER NOT NULL DEFAULT 0 CHECK("is_team" BETWEEN 0 AND 1),
+		"is_team"			INTEGER NOT NULL DEFAULT 0 CHECK("is_team" BETWEEN 0 AND 1),
 		"is_presentation"	INTEGER NOT NULL DEFAULT 0 CHECK("is_presentation" BETWEEN 0 AND 1),
+		"user_id" 			INTEGER NOT NULL,
 		PRIMARY KEY("uid" AUTOINCREMENT),
-		FOREIGN KEY("lecture_id") REFERENCES "lecture"("uid") ON DELETE CASCADE
+		FOREIGN KEY("lecture_id") REFERENCES "lecture"("uid") ON DELETE CASCADE,
+		FOREIGN KEY("user_id") REFERENCES "user"("uid")
 	);
 	`
 	createUserTable = `
 	CREATE TABLE IF NOT EXISTS "user" (
-		"uid"	INTEGER NOT NULL UNIQUE,
-		"id"	TEXT NOT NULL UNIQUE,
-		"pass"	TEXT NOT NULL,
-		"is_cert"	INTEGER,
+		"uid"				INTEGER NOT NULL UNIQUE,
+		"id"				TEXT NOT NULL UNIQUE,
+		"pass"				TEXT NOT NULL,
+		"is_cert"			INTEGER,
 		PRIMARY KEY("uid" AUTOINCREMENT)
 	);
 	`
