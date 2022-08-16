ea7e97858a522a33d7ec98101189b9934db98594
JoonHyug <hyug0819@gmail.com>
2022-08-16 22:13:10 +0900

    review 데이터 모델 분리

--- db/model/review.go
+++ db/model/review.go
@@ -1,1 +1,93 @@
 package model
+
+import (
+	"fmt"
+
+	"github.com/4-ak/sooot/db"
+)
+
+type Review struct {
+	Uid              int
+	Lecture_id       int
+	Writer           int
+	Beneficial_point int //1~5
+	Honey_point      int //1~5
+	Assignment       int //1~3
+	Team_project     int //1~3
+	Pressentation    int //1~3
+	Comment          string
+}
+
+func NewReview() Review {
+	review := Review{}
+	return review
+}
+
+func (r *Review) SelectData(lect_id string) []Review {
+	row, err := db.ReviewAll().Query(lect_id)
+	arr := make([]Review, 0)
+	for row.Next() {
+		row.Scan(
+			&r.Uid,
+			&r.Writer,
+			&r.Lecture_id,
+			&r.Beneficial_point,
+			&r.Honey_point,
+			&r.Assignment,
+			&r.Team_project,
+			&r.Pressentation,
+			&r.Comment)
+		arr = append(arr, *r)
+	}
+	if err != nil || len(arr) == 0 {
+		fmt.Println(err)
+	}
+	return arr
+}
+
+func (r *Review) Insert(lect_id, uid string) {
+	_, err := db.InsertReview().Exec(
+		lect_id,
+		uid,
+		r.Beneficial_point,
+		r.Honey_point,
+		r.Assignment,
+		r.Team_project,
+		r.Pressentation,
+		r.Comment)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (r *Review) Update(uid string) {
+	_, err := db.UpdateReview().Exec(
+		r.Beneficial_point,
+		r.Honey_point,
+		r.Assignment,
+		r.Team_project,
+		r.Pressentation,
+		r.Comment,
+		uid)
+	if err != nil {
+		fmt.Print(err)
+	}
+}
+
+func (r *Review) Delete(uid string) {
+	_, err := db.DeleteReview().Exec(uid)
+	if err != nil {
+		fmt.Print(err)
+	}
+}
+
+func (r *Review) RowData(uid int) {
+	row := db.Review().QueryRow(uid)
+	row.Scan(
+		&r.Beneficial_point,
+		&r.Honey_point,
+		&r.Assignment,
+		&r.Team_project,
+		&r.Pressentation,
+		&r.Comment)
+}
--- handler/review/handler.go
+++ handler/review/handler.go
@@ -4,37 +4,24 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/4-ak/sooot/db"
+	"github.com/4-ak/sooot/db/model"
 	"github.com/gofiber/fiber/v2"
 )
 
 type Handler struct{}
 
-type review struct {
-	Uid              int
-	Lecture_id       int
-	Writer           int
-	Beneficial_point int //1~5
-	Honey_point      int //1~5
-	Assignment       int //1~3
-	Team_project     int //1~3
-	Pressentation    int //1~3
-	Comment          string
-}
-
-var rev review
-
 func (h *Handler) Review(c *fiber.Ctx) error {
-	lectid := (c.Params("id"))
+	review := model.NewReview()
+	lect_id := (c.Params("id"))
 	userid, ok := c.Locals("uid").(string)
 	if !ok {
 		userid = "-1"
 		fmt.Print("userid error")
 	}
-	result := h.SelectData(lectid)
+	review.SelectData(lect_id)
 	return c.Render("review", fiber.Map{
-		"ReviewData": result,
-		"Lectid":     lectid,
+		"ReviewData": review,
+		"Lectid":     lect_id,
 		"Userid":     userid,
 	})
 }
@@ -46,100 +33,36 @@ func (h *Handler) Create(c *fiber.Ctx) error {
 }
 
 func (h *Handler) Update(c *fiber.Ctx) error {
+	review := model.NewReview()
 	uid, _ := strconv.Atoi(c.Params("uid"))
+	review.RowData(uid)
 	return c.Render("editreview", fiber.Map{
-		"ReviewData": h.RowData(uid),
+		"ReviewData": review,
 		"isUpdate":   true,
 	})
 }
 
-func (h *Handler) RowData(uid int) review {
-	row := db.Review().QueryRow(uid)
-	row.Scan(
-		&rev.Beneficial_point,
-		&rev.Honey_point,
-		&rev.Assignment,
-		&rev.Team_project,
-		&rev.Pressentation,
-		&rev.Comment)
-	return rev
-}
-
 func (h *Handler) InsertData(c *fiber.Ctx) error {
+	review := model.NewReview()
 	lect_id := (c.Params("id"))
-	c.BodyParser(&rev)
-	_, err := db.InsertReview().Exec(
-		lect_id,
-		c.Locals("uid").(string),
-		rev.Beneficial_point,
-		rev.Honey_point,
-		rev.Assignment,
-		rev.Team_project,
-		rev.Pressentation,
-		rev.Comment)
-	if err != nil {
-		return c.SendString(err.Error())
-	}
+	c.BodyParser(&review)
+	review.Insert(lect_id, c.Locals("uid").(string))
 	return c.Redirect("/review/" + lect_id)
 }
 
-func (h *Handler) SelectData(lectid string) []review {
-	row, err := db.ReviewAll().Query(lectid)
-	arr := make([]review, 0)
-	for row.Next() {
-		row.Scan(
-			&rev.Uid,
-			&rev.Writer,
-			&rev.Lecture_id,
-			&rev.Beneficial_point,
-			&rev.Honey_point,
-			&rev.Assignment,
-			&rev.Team_project,
-			&rev.Pressentation,
-			&rev.Comment)
-		arr = append(arr, rev)
-	}
-	if err != nil || len(arr) == 0 {
-		fmt.Println(err)
-	}
-	return arr
-}
-
 func (h *Handler) UpdateData(c *fiber.Ctx) error {
+	review := model.NewReview()
 	uid := c.Params("uid")
 	lect_id := c.Params("lectid")
-	c.BodyParser(&rev)
-	_, err := db.UpdateReview().Exec(
-		rev.Beneficial_point,
-		rev.Honey_point,
-		&rev.Assignment,
-		&rev.Team_project,
-		&rev.Pressentation,
-		&rev.Comment,
-		uid)
-	if err != nil {
-		fmt.Print(err)
-		return c.Format(fmt.Sprintf(`
-		<head>
-			<meta charset="UTF-8">
-			<script>
-				if(!alert("값을 다시 입력해주세요")) {
-					window.location="/review/%v/%v/u";
-				}
-			</script>
-		</head>
-		`, lect_id, uid))
-	}
+	c.BodyParser(&review)
+	review.Update(uid)
 	return c.Redirect("/review/" + lect_id)
 }
 
 func (h *Handler) DeleteData(c *fiber.Ctx) error {
+	review := model.NewReview()
 	uid := c.Params("uid")
 	lect_id := c.Params("lectid")
-	_, err := db.DeleteReview().Exec(uid)
-	if err != nil {
-		fmt.Print(err)
-		return c.SendString("DELETE ERROR")
-	}
+	review.Delete(uid)
 	return c.Redirect("/review/" + lect_id)
 }
